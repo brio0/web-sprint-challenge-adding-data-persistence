92f60ea5ce6945444a55a97e021df7cb73d881f1
Bryan Andres <bry.andres001@gmail.com>
2023-05-16 02:04:40 -1000

    first commit, working on model and router

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,23 @@
 // build your `/api/projects` router here
+const express = require('express')
+const Project = require('./model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Project.find()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "error fetching projects",
+                err: err.message,
+                stack: err.stack
+            })
+        })
+})
+
+module.exports = router
+
+
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // start your server here
+const server = require('./api/server')
+
+server.listen(9000, () => {
+    console.log('server has started on http://localhost:9000')
+})
+

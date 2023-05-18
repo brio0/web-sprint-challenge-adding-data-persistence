74e1bf0145410163cbb60cc587721a58626625d5
Bryan Andres <bry.andres001@gmail.com>
2023-05-17 17:02:19 -1000

    project posts

--- api/project/model.js
+++ api/project/model.js
@@ -2,11 +2,23 @@
 const db = require('../../data/dbConfig.js')
 
 module.exports = {
-    find
+    find,
+    findBy,
+    postProject
 }
 
 async function find() {
     const rows = await db('projects')
-    console.log(rows)
     return rows
+}
+
+async function findBy(projectId) {
+    const result = await db('projects').where('project_id', projectId).first()
+    return result
+}
+
+function postProject(project) {
+    return db("projects")
+        .insert(project)
+        .then(([id]) => findBy(id));
 }
--- api/project/router.js
+++ api/project/router.js
@@ -7,7 +7,11 @@ const router = express.Router()
 router.get('/', (req, res) => {
     Project.find()
         .then(project => {
-            res.status(200).json(project)
+            if (project) {
+                res.status(200).json(project)
+            } else {
+                return []
+            }
         })
         .catch(err => {
             res.status(500).json({
@@ -16,6 +20,52 @@ router.get('/', (req, res) => {
                 stack: err.stack
             })
         })
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const project = await Project.findBy(id)
+        if (project) {
+            res.status(200).json(project)
+        } else {
+            res.status(404).json({
+                message: `no project with id: ${id}`
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "error fetching project",
+            err: err.message,
+            stack: err.stack
+        })
+    }
+})
+
+router.post('/', (req, res) => {
+    const { project_name, project_description, project_completed } = req.body
+
+    if (!project_name) {
+        res.status(400).json({
+            message: "Please provide project_name"
+        })
+    } else {
+        Project.postProject({ project_name, project_description, project_completed })
+            .then((project) => {
+                res.status(200).json({
+                    project_name,
+                    project_description,
+                    project_completed
+                })
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "error posting project",
+                    err: err.message,
+                    stack: err.stack
+                })
+            })
+    }
 })
 
 module.exports = router
